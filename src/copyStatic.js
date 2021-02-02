b772e3a6fe35619a9a6700fddc3e9003c4b37602
Burak GUL <burak.gul@ozguryazilim.com.tr>
2021-02-02 22:26:22 +0300

    prepare static file copy function.

--- src/copyStatic.js
+++ src/copyStatic.js
@@ -7,4 +7,24 @@ function catchError(err) {
 }
 
 module.exports = function () {
+  fs.mkdirSync(
+    process.cwd() + "/clockanilla/assets",
+    { recursive: true },
+    catchError
+  );
+  fs.copyFile(
+    __dirname + "/../statics/index.html",
+    process.cwd() + "/clockanilla/index.html",
+    catchError
+  );
+  fs.copyFile(
+    __dirname + "/../statics/assets/index.js",
+    process.cwd() + "/clockanilla/assets/index.js",
+    catchError
+  );
+  fs.copyFile(
+    __dirname + "/../statics/assets/index.css",
+    process.cwd() + "/clockanilla/assets/index.css",
+    catchError
+  );
 }
